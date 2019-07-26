34637b9c402b25b6b24c6cf7f532fdd88a0bc18f
bchain618 <bchain618@gmail.com>
2019-07-26 10:56:53 +0800

    feature#add support for wasm contract
    1. add wasm contract exec logic
    2. use different verify logic based on the code type of contract

--- filter/block/worker.go
+++ filter/block/worker.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"github.com/DSiSc/craft/log"
 	"github.com/DSiSc/craft/types"
-	"github.com/DSiSc/evm-NG"
-	"github.com/DSiSc/gossipswitch/util"
 	"github.com/DSiSc/repository"
 	vcommon "github.com/DSiSc/validator/common"
 	"github.com/DSiSc/validator/tools/merkle_tree"
@@ -16,18 +14,18 @@ import (
 )
 
 type Worker struct {
-	block           *types.Block
-	chain           *repository.Repository
-	receipts        types.Receipts
-	logs            []*types.Log
-	verifySignature bool
+	block     *types.Block
+	chain     *repository.Repository
+	receipts  types.Receipts
+	logs      []*types.Log
+	signature bool
 }
 
-func NewWorker(chain *repository.Repository, block *types.Block, verifySignature bool) *Worker {
+func NewWorker(chain *repository.Repository, block *types.Block, signVerify bool) *Worker {
 	return &Worker{
-		block:           block,
-		chain:           chain,
-		verifySignature: verifySignature,
+		block:     block,
+		chain:     chain,
+		signature: signVerify,
 	}
 }
 
@@ -41,27 +39,21 @@ func GetTxsRoot(txs []*types.Transaction) types.Hash {
 }
 
 func (self *Worker) VerifyBlock() error {
-	// Get previous block
-	previousBlock, err := self.chain.GetBlockByHash(self.block.Header.PrevBlockHash)
-	if err != nil {
-		log.Error("Failed to validate previous block, as: %v", err)
-		return fmt.Errorf("failed to get previous block, as:%v", err)
-	}
-
 	// 1. chainID
-	if self.block.Header.ChainID != previousBlock.Header.ChainID {
+	currentBlock := self.chain.GetCurrentBlock()
+	if self.block.Header.ChainID != currentBlock.Header.ChainID {
 		return fmt.Errorf("wrong Block.Header.ChainID, expected %d, got %d",
-			previousBlock.Header.ChainID, self.block.Header.ChainID)
+			currentBlock.Header.ChainID, self.block.Header.ChainID)
 	}
 	// 2. hash
-	if self.block.Header.PrevBlockHash != previousBlock.HeaderHash {
+	if self.block.Header.PrevBlockHash != currentBlock.HeaderHash {
 		return fmt.Errorf("wrong Block.Header.PrevBlockHash, expected %x, got %x",
-			previousBlock.HeaderHash, self.block.Header.PrevBlockHash)
+			currentBlock.HeaderHash, self.block.Header.PrevBlockHash)
 	}
 	// 3. height
-	if self.block.Header.Height != previousBlock.Header.Height+1 {
+	if self.block.Header.Height != self.chain.GetCurrentBlockHeight()+1 {
 		return fmt.Errorf("wrong Block.Header.Height, expected %x, got %x",
-			previousBlock.Header.Height+1, self.block.Header.Height)
+			self.chain.GetCurrentBlockHeight()+1, self.block.Header.Height)
 	}
 	// 4. txhash
 	txsHash := GetTxsRoot(self.block.Transactions)
@@ -70,15 +62,13 @@ func (self *Worker) VerifyBlock() error {
 			txsHash, self.block.Header.TxRoot)
 	}
 	//5. header hash
-	var defaultHash types.Hash
-	if !bytes.Equal(defaultHash[:], self.block.HeaderHash[:]) {
+	if !(self.block.HeaderHash == types.Hash{}) {
 		headerHash := vcommon.HeaderHash(self.block)
 		if self.block.HeaderHash != headerHash {
 			return fmt.Errorf("wrong Block.HeaderHash, expected %x, got %x",
 				headerHash, self.block.HeaderHash)
 		}
 	}
-
 	var (
 		receipts types.Receipts
 		allLogs  []*types.Log
@@ -101,10 +91,9 @@ func (self *Worker) VerifyBlock() error {
 		log.Debug("Record tx %x receipt is %x.", t.TxHash, common.ReceiptHash(t))
 	}
 	receiptHash := merkle_tree.ComputeMerkleRoot(receiptsHash)
-	var tempHash types.Hash
-	if !bytes.Equal(tempHash[:], self.block.Header.ReceiptsRoot[:]) {
+	if !(self.block.Header.ReceiptsRoot == types.Hash{}) {
 		log.Warn("Receipts root has assigned with %x.", self.block.Header.ReceiptsRoot)
-		if !bytes.Equal(receiptHash[:], self.block.Header.ReceiptsRoot[:]) {
+		if !(receiptHash == self.block.Header.ReceiptsRoot) {
 			log.Error("Receipts root has assigned with %x, but not consistent with %x.",
 				self.block.Header.ReceiptsRoot, receiptHash)
 			return fmt.Errorf("receipts hash not consistent")
@@ -113,24 +102,16 @@ func (self *Worker) VerifyBlock() error {
 		log.Debug("Assign receipts hash %x to block %d.", receiptHash, self.block.Header.Height)
 		self.block.Header.ReceiptsRoot = receiptHash
 	}
-
-	// 7. verify state root
-	stateRoot := self.chain.IntermediateRoot(false)
-	if !bytes.Equal(self.block.Header.StateRoot[:], stateRoot[:]) {
-		log.Warn("The calculated StateRoot is inconsistent with the expected value, calculated: %x, expected: %x", stateRoot, self.block.Header.StateRoot)
-		return fmt.Errorf("state root is inconsistent")
-	}
-
-	// 8. verify digest if it exists
-	if !bytes.Equal(defaultHash[:], self.block.Header.MixDigest[:]) {
+	// 7. verify digest if it exists
+	if !(self.block.Header.MixDigest == types.Hash{}) {
 		digestHash := vcommon.HeaderDigest(self.block.Header)
 		if !bytes.Equal(digestHash[:], self.block.Header.MixDigest[:]) {
 			log.Error("Block digest not consistent which assignment is [%x], while compute is [%x].",
 				self.block.Header.MixDigest, digestHash)
-			return fmt.Errorf("digest not consistent")
+			return fmt.Errorf("digest not in coincidence")
 		}
 	}
-
+	// TODO 8. verify state root
 	self.receipts = receipts
 	self.logs = allLogs
 
@@ -139,16 +120,14 @@ func (self *Worker) VerifyBlock() error {
 
 func (self *Worker) VerifyTransaction(author types.Address, gp *common.GasPool, header *types.Header,
 	tx *types.Transaction, usedGas *uint64) (*types.Receipt, uint64, error) {
-	// verify tx's signature
-	if self.verifySignature {
+	// txs signature has been verified by tx switch already, so ignore it here
+	if self.signature {
 		if self.VerifyTrsSignature(tx) == false {
 			log.Error("Transaction signature verify failed.")
 			return nil, 0, fmt.Errorf("transaction signature failed")
 		}
 	}
-	context := evm.NewEVMContext(*tx, header, self.chain, author)
-	evmEnv := evm.NewEVM(context, self.chain)
-	_, gas, failed, err, contractAddr := ApplyTransaction(evmEnv, tx, gp)
+	_, gas, failed, err, contractAddress := ApplyTransaction(author, header, self.chain, tx, gp)
 	if err != nil {
 		log.Error("Apply transaction %x failed with error %v.", vcommon.TxHash(tx), err)
 		return nil, 0, err
@@ -164,19 +143,14 @@ func (self *Worker) VerifyTransaction(author types.Address, gp *common.GasPool, 
 	receipt.GasUsed = gas
 	// if the transaction created a contract, store the creation address in the receipt.
 	if tx.Data.Recipient == nil {
-		receipt.ContractAddress = contractAddr
 		log.Info("Create contract with address %x within tx %x.", receipt.ContractAddress, receipt.TxHash)
+		receipt.ContractAddress = contractAddress
 	}
 	// Set the receipt logs and create a bloom for filtering
-	receipt.Logs = self.chain.GetLogs(vcommon.TxHash(tx))
-	receipt.Bloom = util.CreateBloom(types.Receipts{receipt})
+	receipt.Logs = self.chain.GetLogs(receipt.TxHash)
+	receipt.Bloom = vcommon.CreateBloom(types.Receipts{receipt})
 
 	return receipt, gas, err
-}
-
-func (self *Worker) GetReceipts() types.Receipts {
-	log.Debug("Get receipts.")
-	return self.receipts
 }
 
 func (self *Worker) VerifyTrsSignature(tx *types.Transaction) bool {
@@ -194,4 +168,9 @@ func (self *Worker) VerifyTrsSignature(tx *types.Transaction) bool {
 		return false
 	}
 	return true
+}
+
+func (self *Worker) GetReceipts() types.Receipts {
+	log.Debug("Get receipts.")
+	return self.receipts
 }
